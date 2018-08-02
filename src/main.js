433eb9d302ea19482e340b27bfcf7baa8ef29b76
Josh Robbs <joshrobbs@Zuul.local>
2018-08-02 15:26:20 -0600

    updated with deployed db link

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ let add = document.getElementById('append')
 
 button.addEventListener('click', (e) => {
   add.innerText = ''
-  fetch('http://localhost:3000/yeast')
+  fetch('https://cakedoughnut.herokuapp.com/yeast')
   .then((res) => res.json())
   .then((data) => {
     data.forEach((item) => {
